f4b1a9a2ca24cb390972a26f3db5bbd7e8a671d9
ryanwalker64 <ryan.walkerz46@gmail.com>
2022-06-04 14:42:32 +1200

    ui

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('hell')
+const caluculatorBox = document.querySelector('#calculator-wrapper');
+
+function add(number1, number2) {
+    const result = number1 + number2;
+    console.log(result);
+}
+
+function subtract(number1, number2) {
+    const result = number1 - number2;
+    console.log(result);
+}
+
+function multiply(number1, number2) {
+    const result = number1 * number2;
+    console.log(result);
+}
+
+function divide(number1, number2) {
+    const result = number1 / number2;
+    console.log(result);
+}
+
+// create the calculator itself
+// add function
+// subtract function
+// mult function
+// divide function
